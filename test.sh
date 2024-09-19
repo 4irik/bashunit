829a646ffaf3b62d643ebe538ac51f71fbb21e07
gs <chekirilla@gmail.com>
2024-09-19 19:12:17 +0700

    feat: out errors after tests result

--- test.sh
+++ test.sh
@@ -1,32 +1,45 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
-COLOR_FAIL='\033[0;31m'
-COLOR_SUCCESS='\033[0;32m'
+TEST_FILE_FILTER=$@
+
+COLOR_FAIL='\033[0;31m' # Red
+COLOR_SUCCESS='\033[0;32m' # Green
 COLOR_NC='\033[0m' # No Color
 
 SUCCESS_COUNTER=0
 FAIL_COUNTER=0
 TEST_FILE_COUNTER=0
 
+declare -A FAIL_OUTPUT
 for i in $(find . -type f -name "test_*.sh");
 do
 	TEST_FILE_COUNTER=$(( TEST_FILE_COUNTER + 1 ))
 
 	scrip_dir=$(dirname $i)
 	script_file=$(basename $i)
-	out=$(cd $scrip_dir && sh ./$script_file)
+	# bash script.sh < input.txt > output.txt 2> error.log
+	out=$(cd $scrip_dir && bash ./$script_file)
+	result=$?
 
-	if [ $? -ne 0 ];
-    then
+	if [ $result -ne 0 ]; then
+		FAIL_OUTPUT[$i]=$out
 		FAIL_COUNTER=$(( FAIL_COUNTER + 1 ))
-        echo "$i: ${COLOR_FAIL}FAIL${COLOR_NC}"
+        echo -e "$i: ${COLOR_FAIL}FAIL${COLOR_NC}"
     else
 		SUCCESS_COUNTER=$(( SUCCESS_COUNTER + 1 ))
-        echo "$i: ${COLOR_SUCCESS}success${COLOR_NC}"
+        echo -e "$i: ${COLOR_SUCCESS}success${COLOR_NC}"
     fi
 done
 
-echo
+if [ ${#FAIL_OUTPUT[@]} -gt 0 ]; then
+	echo
+	echo "ERRORS:"
+	for i in "${!FAIL_OUTPUT[@]}"; do
+		echo -e "${i}:\n${FAIL_OUTPUT[$i]}"
+		echo
+	done
+fi
+
 echo "RESULT:"
 echo "total tests run: $TEST_FILE_COUNTER"
 echo "successfully: $SUCCESS_COUNTER"
